c555453d171f89effcd3a15407dbcb002a3cfdea
Brandy Mello <brandyamello@gmail.com>
2019-10-08 15:35:46 -0600

    Add projects and palettes data, color data

---
+++ color_data.js
@@ -0,0 +1,95 @@
+const colorData = [
+  { name: 'Warm Colors',
+    palettes: [
+      {name: 'Autumn',
+        projectName: 'Warm Colors',
+        colorOne: '#BC8F8F',
+        colorTwo: '#A52A2A',
+        colorThree: '#FF7D40',
+        colorFour: '#CD3700',
+        colorFive: '#993300'
+      },
+      { name: 'Canyon',
+        projectName: 'Warm Colors',
+        colorOne: '#6E352C',
+        colorTwo: '#CF5230',
+        colorThree: '#F59A44',
+        colorFour: '#E3C598',
+        colorFive: '#8A6E64'
+      },
+      { name: 'Spiced',
+        projectName: 'Warm Colors',
+        colorOne: '#F7E5D4',
+        colorTwo: '#FADDAF',
+        colorThree: '#EB712F',
+        colorFour: '#91371B',
+        colorFive: '#472C25'}
+    ]
+  },
+  {
+    name: 'Cool Colors',
+    palettes: [
+      {
+        name: 'SeaBreeze',
+        projectName: 'Cool Colors',
+        colorOne: '#013163',
+        colorTwo: '#015E7E',
+        colorThree: '#00AAB2',
+        colorFour: '#C5BBCC',
+        colorFive: '#A96B70'
+      },
+      {
+        name: 'Lilac',
+        projectName: 'Cool Colors',
+        colorOne: '#C7BAB1',
+        colorTwo: '#BEBCC1',
+        colorThree: '#A3CCF4',
+        colorFour: '#6389DF',
+        colorFive: '#1F2B6C'
+      },
+      {
+        name: 'Antique',
+        projectName: 'Cool Colors',
+        colorOne: '#FEEAE2',
+        colorTwo: '#C2A7A1',
+        colorThree: '#323131',
+        colorFour: '#7B8988',
+        colorFive: '#2F6061'
+      }
+    ]
+  },
+  {
+    name: 'Neutral Colors',
+    palettes: [
+      {
+        name: 'Grounds',
+        projectName: 'Neutral Colors',
+        colorOne: '#EBE9E7',
+        colorTwo: '#C4BDB7',
+        colorThree: '#8A7B70',
+        colorFour: '#4F3928',
+        colorFive: '#3C2411'
+      },
+      {
+        name: 'Aged Copper',
+        projectName: 'Neutral Colors',
+        colorOne: '#41434A',
+        colorTwo: '#6E9489',
+        colorThree: '#DEDCC3',
+        colorFour: '#F2F1E9',
+        colorFive: '#877963'
+      },
+      {
+        name: 'Stone',
+        projectName: 'Neutral Colors',
+        colorOne: '#D2D4DC',
+        colorTwo: '#AFAFAF',
+        colorThree: '#F8F8FA',
+        colorFour: '#E5E6EB',
+        colorFive: '#C0C2CE'
+      }
+    ]
+  }
+]
+
+module.exports = colorData;
