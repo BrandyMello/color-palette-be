157b669c64da0809ab3e4cab0856173baba4635e
Brandy Mello <brandyamello@gmail.com>
2019-10-09 15:42:20 -0600

    Set up tests and add root path test and get projects test

--- app.test.js
+++ app.test.js
@@ -1,3 +1,5 @@
+const request = require('supertest');
+const app =  require('./app');
 const environment = process.env.NODE_ENV || "development";
 const configuration = require("./knexfile")[environment];
 const database = require("knex")(configuration);
@@ -9,8 +11,19 @@ describe('Server', () => {
 
   describe('init', () => {
     it('should return a 200 status code and show a connection to root', async () =>{
-      const res = await requestAnimationFrame(app).get('/');
+      const res = await request(app).get('/');
       expect(res.status).toBe(200);
     });
   });
-})
+
+  describe('GET /projects', () => {
+    it('should return a 200 status code and all the projects', async () => {
+      const expectedProjects = await database('projects').select();
+      const response = await request(app).get('/projects');
+      const projects = response.body;
+
+      expect(response.status).toBe(200);
+      expect(projects).toEqual(expectedProjects);
+    });
+  });
+});
