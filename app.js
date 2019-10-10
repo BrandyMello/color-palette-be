3bca8ab76b9def8f2ffffb9c3a7d510e5d6f1908
Emily <lalonde0902@gmail.com>
2019-10-10 15:48:35 -0600

    Fix package-lock merge issue

--- app.js
+++ app.js
@@ -48,6 +48,105 @@ app.get('/api/v1/palettes/:id', (request, response) => {
         });
       }
     })
+});
+
+app.post('/api/v1/projects', (request, response) => {
+  const project = request.body;
+
+  for (let requiredParameter of ['name']) {
+    if(!project[requiredParameter]) {
+      return response
+        .status(422)
+        .send({
+          error: `Expected format: {
+          name: <String>
+        }. You are missing a "${requiredParameter}".`})
+    }
+  }
+  database('projects').insert(project, 'id')
+    .then(project => {
+      return response.status(201).json({ id: project[0] })
+    })
+    .catch(error => {
+      return response.status(500).json({error})
+    });
+
+    app.delete('/api/v1/projects', (request, response) => {
+      database('projects').where('id', request.params.id).del()
+        .then(project => {
+          if(!project) {
+            response.status(404).send('Project not found');
+          } else {
+            response.status(204).json(`Project with id ${request.params.id} has been deleted from your projects`)
+          }
+        })
+        .catch(error => {
+          response.status(500).json({ error });
+        });
+    });
+});
+
+app.post('/api/v1/palettes', (request, response) => {
+  const palette = request.body;
+
+  for(let requiredParameter of [
+    'name',
+    'projectName',
+    'colorOne',
+    'colorTwo',
+    'colorThree',
+    'colorFour',
+    'colorFive',
+    'projectId'
+  ]) {
+    if(!palette[requiredParameter]) {
+      return response.status(422).send({error: 'Unexpected format'})
+    }
+  }
+  database('palettes').insert(palette, 'id')
+  .then(palette => {
+    return response.status(201).json({id: palette[0]})
 })
+})
+
+// app.patch("/api/v1/palettes/:id", (req, res) => {
+//   if (!req.body.name) {
+//     res.status(422).json("Please enter a name");
+//   } else {
+//     database("palettes")
+//       .where("id", req.params.id)
+//       .update(req.body)
+//       .then(project => {
+//         res.status(201).json({ id: project });
+//       })
+//   }
+// });
+
+app.delete("/api/v1/palettes/:id", (req, res) => {
+  database("palettes")
+    .where("id", req.params.id).del()
+    .then(palette => {
+      if (!palette) {
+        res.status(404).json("No palette found");
+      } else {
+        res
+          .status(204)
+          .json(`Palette with id ${req.params.id} has been deleted.`);
+      }
+    })
+});
+
+app.delete('/api/v1/projects/:id', async (request, response) => {
+  const project = await database('projects').where('id', request.params.id).select();
+  
+  if(project.length) {
+    await database('palettes').where('projectId', request.params.id).del()
+    await database('projects').where('id', request.params.id).del()
+    
+    response.status(200).json('This project has been deleted.')
+  } else {
+    return response.status(400).json({ error: 'Delete failed, could not find project with that id.' })
+  }
+});
 
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -1,25 +1,25 @@
-const request = require('supertest');
-const app =  require('./app');
+const request = require("supertest");
+const app = require("./app");
 const environment = process.env.NODE_ENV || "development";
 const configuration = require("./knexfile")[environment];
 const database = require("knex")(configuration);
 
-describe('Server', () => {
+describe("Server", () => {
   beforeEach(async () => {
-    await database.seed.run()
+    await database.seed.run();
   });
 
-  describe('init', () => {
-    it('should return a 200 status code and show a connection to root', async () =>{
-      const res = await request(app).get('/');
+  describe("init", () => {
+    it("should return a 200 status code and show a connection to root", async () => {
+      const res = await request(app).get("/");
       expect(res.status).toBe(200);
     });
   });
 
-  describe('GET /api/v1/projects', () => {
-    it('should return a 200 status code and all the projects', async () => {
-      const expectedProjects = await database('projects').select();
-      const res = await request(app).get('/api/v1/projects');
+  describe("GET /api/v1/projects", () => {
+    it("should return a 200 status code and all the projects", async () => {
+      const expectedProjects = await database("projects").select();
+      const res = await request(app).get("/api/v1/projects");
       const projects = res.body;
 
       expect(res.status).toBe(200);
@@ -27,22 +27,22 @@ describe('Server', () => {
     });
   });
 
-  describe('GET /api/v1/projects/:id', () => {
-    it('should return a 200 status and a single project if the project exists', async () => {
-      const expectedProject = await database('projects').first();
+  describe("GET /api/v1/projects/:id", () => {
+    it("should return a 200 status and a single project if the project exists", async () => {
+      const expectedProject = await database("projects").first();
       const id = expectedProject.id;
       const res = await request(app).get(`/api/v1/projects/${id}`);
-      const result = res.body[0]
+      const result = res.body[0];
 
       expect(res.status).toBe(200);
       expect(result[0]).toEqual(expectedProject[0]);
     });
   });
 
-  describe('GET /api/v1/palettes', () => {
-    it('should return a 200 status code and all the palettes', async () => {
-      const expectedPalettes = await database('palettes').select();
-      const res = await request(app).get('/api/v1/palettes');
+  describe("GET /api/v1/palettes", () => {
+    it("should return a 200 status code and all the palettes", async () => {
+      const expectedPalettes = await database("palettes").select();
+      const res = await request(app).get("/api/v1/palettes");
       const palettes = res.body;
 
       expect(res.status).toBe(200);
@@ -50,16 +50,114 @@ describe('Server', () => {
     });
   });
 
-  describe('GET /api/v1/palettes/:id', () => {
-    it('should return a 200 status and a single palette if the palette exists', async () => {
-      const expectedPalette = await database('palettes').first();
+  describe("GET /api/v1/palettes/:id", () => {
+    it("should return a 200 status and a single palette if the palette exists", async () => {
+      const expectedPalette = await database("palettes").first();
       const id = expectedPalette.id;
       const res = await request(app).get(`/api/v1/palettes/${id}`);
-      const result = res.body[0]
+      const result = res.body[0];
 
       expect(res.status).toBe(200);
       expect(result[0]).toEqual(expectedPalette[0]);
     });
   });
+
+  describe('Post /api/v1/palettes', () => {
+    it('should return a 201 status code and add a new project to the database', async () => {
+      const paletteData = await database('palettes').select()
+      const id = paletteData[0].projectId
+      const mockData = {
+              name: 'Autumn',
+              projectName: 'Warm Colors',
+              colorOne: '#BC8F8F',
+              colorTwo: '#A52A2A',
+              colorThree: '#FF7D40',
+              colorFour: '#CD3700',
+              colorFive: '#993300',
+              projectId: id
+            }
+      const response = await request(app).post('/api/v1/palettes').send(mockData);
+      const palettes = await database('palettes').where({ id: response.body.id });
+      const newPalette = palettes[0];
+
+      expect(response.status).toBe(201);
+      expect(newPalette.name).toEqual(mockData.name);
+    });
+  });
+
+  describe("DELETE api/v1/palettes/:id", () => {
+    it("should return a 204 status code and remove palette from database", async () => {
+      const palette = await database("palettes")
+        .first()
+        .select();
+      const id = palette.id;
+      const response = await request(app).delete(`/api/v1/palettes/${id}`);
+      expect(response.status).toBe(204);
+    });
+
+    it("should return a 404 if a request id is bad", async () => {
+      const response = await request(app).delete("/api/v1/palettes/-1");
+      expect(response.status).toBe(404);
+    });
+  });
+
+  describe('DELETE /api/v1/projects/:id', () => {
+    it('should return a 200 and a message', async () => {
+      const project = await database('projects').first();
+      const id = project.id;
+
+      const res = await request(app).delete(`/api/v1/projects/${id}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual('This project has been deleted.')
+    });
+
+    it('should return a 400 and an error message', async () => {
+     const res = await request(app).delete(`/api/v1/projects/-1`);
+
+      expect(res.status).toBe(400);
+      expect(res.body.error).toEqual('Delete failed, could not find project with that id.')
+    });
+  });
 });
 
+  describe('Post /api/v1/projects', () => {
+    it('should return a 201 status code and add a new project to the database', async () => {
+      const mockProject = {name: 'Summer Time'};
+      const response = await request(app).post('/api/v1/projects').send(mockProject);
+      const projects = await database('projects').where({ id: response.body.id });
+      const newProject = projects[0];
+
+      expect(response.status).toBe(201);
+      expect(newProject.name).toEqual(mockProject.name);
+    });
+
+    it('should return a 422 status code with an error message "You are missing "parameter here"." if it does not have all of the required parameters', async () => {
+      const mockProject = {color: 'green'};
+      const response = await request(app).post('/api/v1/projects').send(mockProject);
+
+      expect(response.status).toBe(422);
+      expect(response.body.error).toEqual(`Expected format: {
+          name: <String>
+        }. You are missing a "name".`)
+        });
+      });
+
+      describe('DELETE /api/v1/projects/:id', () => {
+        it('should return a 200 status code and remove the project with the given id from the projects database', async () => {
+          const projectId = await database('projects').first('id').then(project => project.id); 
+          console.log('projectid', projectId)
+          const response = await request(app).delete(`/api/v1/projects/${projectId}`);
+          // console.log('db', database('projects'))
+
+          expect(response.status).toBe(200);
+          // expect(response.body.error).toEqual('Project not found')
+        })
+
+        it('should return a 404 if the project is not found', async () => {
+         const response = await request(app).delete('/api/v1/projects/-1')
+         expect(response.status).toBe(400) 
+        })
+      });
+ 
+    
