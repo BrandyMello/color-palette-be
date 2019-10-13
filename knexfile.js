636d03bf0082c80c4478f73d6a35e3a966672274
Brandy Mello <brandyamello@gmail.com>
2019-10-13 12:55:12 -0600

    Add procfile and update knex file for production

--- knexfile.js
+++ knexfile.js
@@ -26,4 +26,13 @@ module.exports = {
     useNullAsDefault: true
   },
 
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+    directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
+
 };
